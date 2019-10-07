ece01c27850077dfb3939ba7af882f53077f276d
Fabian Conejo <fdconejop@unal.edu.co>
2019-10-07 05:03:48 -0700

    Concatenation, Join, Format and Base64 obfuscation

--- PowerShell/Tool/StringObfuscator.ps1
+++ PowerShell/Tool/StringObfuscator.ps1
@@ -1,3 +1,5 @@
+Using module ".\Utility.psm1"
+
 Class StringObfuscator{
 
     static [String] Obfuscate([String] $Word){
@@ -21,7 +23,7 @@ Class StringObfuscator{
             [Object[]] $SplittedQuotedWordsObf = [StringObfuscator]::SplitIntoQuotedObfStrings($Word, 2)
             return "(" + $SplittedQuotedWordsObf[0] + "+" + $SplittedQuotedWordsObf[1] + ")"
         }
-        return $Word;
+        return [StringObfuscator]::TerminalNode($Word)
     }
         
     static [String] JoinObfuscation([String] $Word){
@@ -31,7 +33,7 @@ Class StringObfuscator{
             [String] $JoinType = [Utility]::pickOneRandom($JoinTypes)
             return $JoinType -f [StringObfuscator]::SplitIntoQuotedObfStrings($Word, 2)
         }
-        return $Word;
+        return [StringObfuscator]::TerminalNode($Word)
     }
 
 
@@ -41,42 +43,45 @@ Class StringObfuscator{
             [Object[]] $SplittedQuotedWordsObf = [StringObfuscator]::SplitIntoQuotedObfStrings($Word, 2)
             return "(`"{1}{0}`"-f " + ("{0},{1})" -f $SplittedQuotedWordsObf[1], $SplittedQuotedWordsObf[0])
         }
-        return $Word;
+        return [StringObfuscator]::TerminalNode($Word)
     }
 
     static [String] Base64Obfuscation([String] $Word){
         [Int] $WordLength = $Word.Length;
         if($WordLength -ge 8){
-            [String] $Content = $Word.Substring(1, $WordLength - 2)
-            [String] $Base64String = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($Content))
-            return "[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('{0}'))" -f $Base64String
+            #[String] $Content = $Word.Substring(1, $WordLength - 2)
+            [String] $Base64String = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($Word))
+            return "[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String({0}))" -f [StringObfuscator]::TerminalNode($Base64String)
         }
-        return $Word;
+        return [StringObfuscator]::TerminalNode($Word)
         
     }
 
     ## [END] Obfuscation techniques
 
-
-    static [String] quoteString($Word, $QuoteType){
-        return "$QuoteType$Word$QuoteType";
+    
+    static [String] TerminalNode([String] $Word){
+        return "`"" + [StringObfuscator]::SanetizeWord($Word) + "`"";
     }
 
-
+    static [String] SanetizeWord([String] $Word){
+        $Word = $Word.Replace('"',"`'")
+        $Word = $Word.Replace('.',"`.")
+        return $Word
+    }
+    
     static [Object[]] SplitIntoQuotedStrings([String] $Word, [Int] $Splits){
         [Int] $WordLength = $Word.Length;
-        [String] $Content = $Word.Substring(1, $WordLength - 2)
-        [Int] $ContentLength = $Content.Length;
 
         $SplitSizes =  @();
-        $SplitMeanSize = [int] ($ContentLength / $Splits);
+        $SplitMeanSize = [int] ($WordLength / $Splits);
         $ContentSizeUsed = 0;
         for($i = 1; $i -lt $Splits; $i++){
             $SplitSize = Get-Random -Minimum 1 -Maximum $SplitMeanSize
             $SplitSizes += ,$SplitSize
             $ContentSizeUsed += $SplitSize
         }
-        $SplitSizes += ,($ContentLength - $ContentSizeUsed)
+        $SplitSizes += ,($WordLength - $ContentSizeUsed)
 
         # Shuffle split sizes
         $SplitSizes = $SplitSizes | Sort-Object {Get-Random}
@@ -84,8 +89,8 @@ Class StringObfuscator{
         $SplitQuotedStrings = @();
         $SplitOffSet = 0;
         foreach($SplitSize in $SplitSizes){
-            $ContentSubstring = $Content.Substring($SplitOffSet, $SplitSize).Replace("`'","-")
-            $SplitQuotedStrings += ,("`'" + $ContentSubstring + "`'") 
+            $WordSubstring = $Word.Substring($SplitOffSet, $SplitSize)
+            $SplitQuotedStrings += ,$WordSubstring 
             $SplitOffSet += $SplitSize
         }
         return $SplitQuotedStrings;
@@ -94,9 +99,6 @@ Class StringObfuscator{
 
     static [Object[]] SplitIntoQuotedObfStrings([String] $Word, [Int] $Splits){
         [Object[]] $SplittedQuotedWords = [StringObfuscator]::SplitIntoQuotedStrings($Word, 2)
-        return $SplittedQuotedWords | % {[StringObfuscator]::Obfuscate($_)}
+        return $SplittedQuotedWords | ForEach-Object {[StringObfuscator]::Obfuscate($_)}
     }
-
-
-
 }
