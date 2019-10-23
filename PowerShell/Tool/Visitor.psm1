137488c92055a1264001b3c020d4e663f3644210
Fabian Conejo <fdconejop@unal.edu.co>
2019-10-23 11:12:00 -0700

    More statements implemented

--- PowerShell/Tool/Visitor.psm1
+++ PowerShell/Tool/Visitor.psm1
@@ -1,5 +1,6 @@
 Using module '.\StringHelper.psm1'
 Using module '.\StringObfuscator.psm1'
+Using module '.\Tokens.psm1'
 
 class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
                 System.Management.Automation.Language.ICustomAstVisitor2
@@ -41,10 +42,14 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     }
 
     [system.object] VisitArrayExpression([System.Management.Automation.Language.ArrayExpressionAst] $statement){
+        $this.NewCode.Append("@").NoSpace().Append("(")
+        $this.VisitElement($statement.SubExpression)
+        $this.NewCode.Append(")")
         return $statement
     }
     
     [system.object] VisitArrayLiteral([System.Management.Automation.Language.ArrayLiteralAst] $statement){
+        $this.VisitElements($statement.Elements, ",")
         return $statement
     }
     
@@ -56,6 +61,9 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     }
     
     [system.object] VisitAttribute([System.Management.Automation.Language.AttributeAst] $statement){
+        $this.NewCode.Append("[").Append($statement.Typename.Name).Append("(")
+        $this.VisitElements($statement.NamedArguments, ",")
+        $this.NewCode.Append(")").Append("]")
         return $statement
     }
     
@@ -68,6 +76,10 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     }
     
     [system.object] VisitBinaryExpression([System.Management.Automation.Language.BinaryExpressionAst] $statement){
+        $this.VisitElement($statement.Left)
+        $token = [Tokens]::Kind[$statement.Operator.ToString()]
+        $this.NewCode.Append($token)
+        $this.VisitElement($statement.Right)
         return $statement
     }
     
@@ -85,10 +97,7 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     
     [system.object] VisitCommand([System.Management.Automation.Language.CommandAst] $statement){
         If($statement.InvocationOperator){
-            #System.Management.Automation.Language.TokenKind
-            #TODO : Create token Dictionary
-            #       https://docs.microsoft.com/en-us/dotnet/api/system.management.automation.language.tokenkind?view=pscore-6.2.0
-            $Token = "&" # Get the correct token using dictionary
+            $token = [Tokens]::Kind[$statement.InvocationOperator.ToString()]
             $this.NewCode.Append($Token).NoSpace()
         }
         $this.VisitElements($statement.CommandElements)
@@ -119,6 +128,8 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     }
     
     [system.object] VisitConvertExpression([System.Management.Automation.Language.ConvertExpressionAst] $statement){
+        $this.VisitElement($statement.Type)
+        $this.VisitElement($statement.Child)
         return $statement
     }
     
@@ -127,10 +138,20 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     }
     
     [system.object] VisitDoUntilStatement([System.Management.Automation.Language.DoUntilStatementAst] $statement){
+        $this.NewCode.Append("Do").Append("{")
+        $this.VisitElement($statement.Body)
+        $this.NewCode.Append("}").Append("Until").Append("(")
+        $this.VisitElement($statement.Condition)
+        $this.NewCode.Append(")")
         return $statement
     }
     
     [system.object] VisitDoWhileStatement([System.Management.Automation.Language.DoWhileStatementAst] $statement){
+        $this.NewCode.Append("Do").Append("{")
+        $this.VisitElement($statement.Body)
+        $this.NewCode.Append("}").Append("While").Append("(")
+        $this.VisitElement($statement.Condition)
+        $this.NewCode.Append(")")
         return $statement
     }
 
@@ -151,6 +172,28 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     }
     
     [system.object] VisitExpandableStringExpression([System.Management.Automation.Language.ExpandableStringExpressionAst] $statement){
+        $StringConstantType = $statement.StringConstantType
+        $QuoteTypeStart = ""
+        $QuoteTypeEnd = ""
+        $NewValue = $statement.Value
+        If($StringConstantType -eq [System.Management.Automation.Language.StringConstantType]::SingleQuoted){
+            $QuoteTypeStart = $QuoteTypeEnd = "'"
+            #$NewValue = [StringObfuscator]::Obfuscate($statement.Value)
+        }ElseIf($StringConstantType -eq [System.Management.Automation.Language.StringConstantType]::SingleQuotedHereString){
+            $QuoteTypeStart = "@'"
+            $QuoteTypeEnd = "'@"
+        }ElseIf($StringConstantType -eq [System.Management.Automation.Language.StringConstantType]::DoubleQuoted){
+            $QuoteTypeStart = $QuoteTypeEnd = "`""
+            #$NewValue = [StringObfuscator]::Obfuscate($statement.Value)
+        }ElseIf($StringConstantType -eq [System.Management.Automation.Language.StringConstantType]::DoubleQuotedHereString){
+            $QuoteTypeStart = "@`""
+            $QuoteTypeEnd = "`"@"
+        }ElseIf($StringConstantType -eq [System.Management.Automation.Language.StringConstantType]::BareWord){
+            $QuoteTypeStart = $QuoteTypeEnd = ""
+            $NewValue = [StringObfuscator]::CapitalisationObfuscation($statement.Value)
+        }
+        #$QuoteTypeStart = $QuoteTypeEnd = ""
+        $this.NewCode.Append($QuoteTypeStart).NoSpace().Append($NewValue).NoSpace().Append($QuoteTypeEnd)
         return $statement
     }
     
@@ -159,14 +202,33 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     }
     
     [system.object] VisitForEachStatement([System.Management.Automation.Language.ForEachStatementAst] $statement){
+        $this.NewCode.Append("ForEach").Append("(")
+        $this.VisitElement($statement.Variable)
+        $this.NewCode.Append("in")
+        $this.VisitElement($statement.Condition)
+        $this.NewCode.Append(")").Append("{")
+        $this.VisitElement($statement.Body)
+        $this.NewCode.Append("}")
         return $statement
     }
     
     [system.object] VisitForStatement([System.Management.Automation.Language.ForStatementAst] $statement){
+        $this.NewCode.Append("For").Append("(")
+        $this.VisitElement($statement.Initializer)
+        $this.NewCode.Append(";")
+        $this.VisitElement($statement.Condition)
+        $this.NewCode.Append(";")
+        $this.VisitElement($statement.Iterator)
+        $this.NewCode.Append(")").Append("{")
+        $this.VisitElement($statement.Body)
+        $this.NewCode.Append("}")
         return $statement
     }
     
     [system.object] VisitFunctionDefinition([System.Management.Automation.Language.FunctionDefinitionAst] $statement){
+        $this.NewCode.Append("function").Append($statement.Name).Append("{").NewLine()
+        $this.VisitElement($statement.Body)
+        $this.NewCode.Append("}")
         return $statement
     }
 
@@ -192,6 +254,21 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     }
     
     [system.object] VisitIfStatement([System.Management.Automation.Language.IfStatementAst] $statement){
+        For($i = 0; $i -lt $statement.Clauses.Item1.Length; $i++){
+            If($i -ne 0){
+                $this.NewCode.Append("Else").NoSpace()
+            }
+            $this.NewCode.Append("If").Append("(")
+            $this.VisitElement($statement.Clauses.Item1[$i])
+            $this.NewCode.Append(")").Append("{")
+            $this.VisitElement($statement.Clauses.Item2[$i])
+            $this.NewCode.Append("}")
+        }
+        If($statement.ElseClause){
+            $this.NewCode.Append("Else").Append("{")
+            $this.VisitElement($statement.ElseClause)
+            $this.NewCode.Append("}")
+        }
         return $statement
     }
     
@@ -211,7 +288,13 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     
     [system.object] VisitMemberExpression([System.Management.Automation.Language.MemberExpressionAst] $statement){
         $this.VisitElement($statement.Expression)
-        $this.NewCode.NoSpace().Append(".").NoSpace()
+        $this.NewCode.NoSpace()
+        If($statement.Static){
+            $this.NewCode.Append("::")
+        }Else{
+            $this.NewCode.Append(".")
+        }
+        $this.NewCode.NoSpace()
         $this.VisitElement($statement.Member)
         return $statement
     }
@@ -221,6 +304,8 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     }
     
     [system.object] VisitNamedAttributeArgument([System.Management.Automation.Language.NamedAttributeArgumentAst] $statement){
+        $this.NewCode.Append($statement.ArgumentName).Append("=")
+        $this.VisitElement($statement.Argument)
         return $statement
     }
     
@@ -231,6 +316,10 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     }
     
     [system.object] VisitParamBlock([System.Management.Automation.Language.ParamBlockAst] $statement){
+        $this.VisitElements($statement.Attributes, ",")
+        $this.NewCode.Append("Param").Append("(")
+        $this.VisitElements($statement.Parameters, ",")
+        $this.NewCode.Append(")")
         return $statement
     }
     
@@ -280,17 +369,25 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     
     [system.object] VisitScriptBlock([System.Management.Automation.Language.ScriptBlockAst] $statement){
         $this.VisitElement($statement.BeginBlock)
-        $this.VisitElement($statement.EndBlock)
+        $this.NewCode.NewLine()
         $this.VisitElement($statement.ParamBlock)
+        $this.NewCode.NewLine()
+        $this.VisitElement($statement.EndBlock)
+        $this.NewCode.NewLine()
         $this.VisitElement($statement.ProcessBlock)
+        $this.NewCode.NewLine()
         return $statement
     }
     
     [system.object] VisitScriptBlockExpression([System.Management.Automation.Language.ScriptBlockExpressionAst] $statement){
+        $this.NewCode.Append("{")
+        $this.VisitElement($statement.ScriptBlock)
+        $this.NewCode.Append("}")
         return $statement
     }
     
     [system.object] VisitStatementBlock([System.Management.Automation.Language.StatementBlockAst] $statement){
+        $this.VisitStatements($statement.Statements)
         return $statement
     }
     
@@ -367,10 +464,13 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     }
     
     [system.object] VisitTypeExpression([System.Management.Automation.Language.TypeExpressionAst] $statement){
+        $this.NewCode.Append("[").Append($statement.TypeName.Name).Append("]")
         return $statement
     }
     
     [system.object] VisitUnaryExpression([System.Management.Automation.Language.UnaryExpressionAst] $statement){
+        $this.VisitElement($statement.Child)
+        $this.NewCode.NoSpace().Append([Tokens]::Kind[$statement.TokenKind.ToString()])
         return $statement
     }
     
@@ -393,6 +493,11 @@ class Visitor : System.Management.Automation.Language.ICustomAstVisitor,
     }
     
     [system.object] VisitWhileStatement([System.Management.Automation.Language.WhileStatementAst] $statement){
+        $this.NewCode.Append("While").Append("(")
+        $this.VisitElement($statement.Condition)
+        $this.NewCode.Append(")").Append("{")        
+        $this.VisitElement($statement.Body)
+        $this.NewCode.Append("}")
         return $statement
     }
      
